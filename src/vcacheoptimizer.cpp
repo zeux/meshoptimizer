ce93eaf278454a750a0b375f8464d18ec42dfc47
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2017-08-06 11:20:17 +0100

    vcache: Update tables for linear cache algorithm
    The new tables provide marginally better results (1% relative
    improvement) on both LRU simulation and NVidia hardware.

--- src/vcacheoptimizer.cpp
+++ src/vcacheoptimizer.cpp
@@ -206,6 +206,7 @@ static void optimizeVertexCacheFIFO(unsigned int* destination, const unsigned in
 const size_t max_cache_size = 32;
 const size_t max_valence = 32;
 
+#if 0
 static float vertex_score_table_cache[1 + max_cache_size];
 static float vertex_score_table_live[max_valence];
 
@@ -220,14 +221,31 @@ void vertexScoreInit(float last_triangle_score, float valence_boost_scale, float
 
 	vertex_score_table_live[0] = 0;
 
-	for (size_t i = 1; i < sizeof(vertex_score_table_live) / sizeof(vertex_score_table_live[0]); ++i)
+	for (size_t i = 1; i < max_valence; ++i)
 	{
 		vertex_score_table_live[i] = valence_boost_scale / powf(float(i), valence_boost_power);
 	}
 }
+#else
+// last_triangle_score = 0.8, cache_decay_power = 1.5
+static const float vertex_score_table_cache[1 + max_cache_size] =
+{
+	0.000000f,
+	0.800000f, 0.800000f, 0.800000f, 1.000000f, 0.948724f, 0.898356f, 0.848913f, 0.800411f,
+	0.752870f, 0.706309f, 0.660750f, 0.616215f, 0.572727f, 0.530314f, 0.489003f, 0.448824f,
+	0.409810f, 0.371997f, 0.335425f, 0.300136f, 0.266180f, 0.233610f, 0.202490f, 0.172889f,
+	0.144890f, 0.118591f, 0.094109f, 0.071591f, 0.051226f, 0.033272f, 0.018111f, 0.006403f,
+};
 
-// TODO: figure out optimal values and pregenerate the tables
-static struct VertexScoreInitializer { VertexScoreInitializer() { vertexScoreInit(0.75f, 2.0f, 0.5f, 1.5f); } } vertex_score_table_init;
+// valence_boost_scale = 3.2, valence_boost_power = 0.9
+static const float vertex_score_table_live[1 + max_valence] =
+{
+	0.000000f, 3.200000f, 1.714838f, 1.190531f, 0.918959f, 0.751756f, 0.637990f, 0.555344f,
+	0.492458f, 0.442927f, 0.402856f, 0.369740f, 0.341890f, 0.318127f, 0.297601f, 0.279684f,
+	0.263902f, 0.249888f, 0.237358f, 0.226085f, 0.215885f, 0.206611f, 0.198139f, 0.190368f,
+	0.183215f, 0.176605f, 0.170480f, 0.164787f, 0.159481f, 0.154523f, 0.149879f, 0.145521f,
+};
+#endif
 
 static float vertexScore(int cache_position, unsigned int live_triangles)
 {
