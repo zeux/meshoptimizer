56e0563689ae1ea3faed4cf6cfb548eb5084d0f1
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2017-08-20 18:15:58 -0400

    indexgenerator: Simplify hashing function
    All real-world vertices are 4 bytes aligned so there's no real need to
    process the tail - and it seems like that obviates the need for the
    murmur hash finalizer as well.
    
    Additionally, vertex size is a constant for each set of hash invocations
    so we can start with seed=0.

--- src/indexgenerator.cpp
+++ src/indexgenerator.cpp
@@ -9,19 +9,14 @@
 namespace meshopt
 {
 
-static unsigned int murmurHash2(const void* key, size_t len, unsigned int seed)
+static unsigned int murmurHash(const char* key, size_t len, unsigned int h)
 {
 	const unsigned int m = 0x5bd1e995;
 	const int r = 24;
 
-	unsigned int h = seed ^ static_cast<unsigned int>(len);
-
-	// Mix 4 bytes at a time into the hash
-	const unsigned char* data = (const unsigned char*)key;
-
 	while (len >= 4)
 	{
-		unsigned int k = *(unsigned int*)data;
+		unsigned int k = *reinterpret_cast<const unsigned int*>(key);
 
 		k *= m;
 		k ^= k >> r;
@@ -30,28 +25,9 @@ static unsigned int murmurHash2(const void* key, size_t len, unsigned int seed)
 		h *= m;
 		h ^= k;
 
-		data += 4;
+		key += 4;
 		len -= 4;
 	}
-
-	// Handle the last few bytes of the input array
-	switch (len)
-	{
-	case 3:
-		h ^= data[2] << 16;
-	case 2:
-		h ^= data[1] << 8;
-	case 1:
-		h ^= data[0];
-		h *= m;
-	};
-
-	// Do a few final mixes of the hash to ensure the last few
-	// bytes are well-incorporated.
-
-	h ^= h >> 13;
-	h *= m;
-	h ^= h >> 15;
 
 	return h;
 }
@@ -68,7 +44,7 @@ struct VertexHasher
 
 	size_t operator()(unsigned int index) const
 	{
-		return murmurHash2(vertices + index * vertex_size, vertex_size, 0);
+		return murmurHash(vertices + index * vertex_size, vertex_size, 0);
 	}
 
 	size_t operator()(unsigned int lhs, unsigned int rhs) const
