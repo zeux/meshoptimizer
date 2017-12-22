b08331afa4180dd33558e4f8f5debbe63447fdaa
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2017-12-22 10:53:12 -0800

    tools: Add vcachetuner tool
    This can be used to find optimal settings for vertex cache optimization
    tables using a genetic algorithm. The optimization is pretty efficient
    but results depend on whether you want to optimize for all cache
    profiles or just one - as submitted the tool optimizes for all vendors
    equally to try to get cache-oblivious settings.
    
    Note that it uses OpenMP for performance and requires changing vcache
    tables to be thread-local instead of static.

---
+++ tools/vcachetuner.cpp
@@ -0,0 +1,448 @@
+#include "../src/meshoptimizer.h"
+#include "../demo/objparser.h"
+
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <cstdio>
+#include <cstdint>
+#include <cstring>
+#include <thread>
+
+namespace meshopt
+{
+	extern thread_local float vertex_score_table_cache[1 + 32];
+	extern thread_local float vertex_score_table_live[1 + 32];
+}
+
+enum Profile
+{
+	Profile_AMD_GCN,
+	Profile_NVidia,
+	Profile_NVidia_Pascal,
+	Profile_Intel,
+
+	Profile_Count
+};
+
+struct { int cache, warp, triangle; } profiles[Profile_Count] =
+{
+	{ 14, 64, 128 },
+	{ 16, 32, 32 },
+	{ 32, 32, 32 },
+	{ 128, 0, 0 },
+};
+
+typedef struct { uint64_t state;  uint64_t inc; } pcg32_random_t;
+
+#define PCG32_INITIALIZER   { 0x853c49e6748fea9bULL, 0xda3e39cb94b95bdbULL }
+
+uint32_t pcg32_random_r(pcg32_random_t* rng)
+{
+    uint64_t oldstate = rng->state;
+    // Advance internal state
+    rng->state = oldstate * 6364136223846793005ULL + (rng->inc|1);
+    // Calculate output function (XSH RR), uses old state for max ILP
+    uint32_t xorshifted = ((oldstate >> 18u) ^ oldstate) >> 27u;
+    uint32_t rot = oldstate >> 59u;
+    return (xorshifted >> rot) | (xorshifted << ((-rot) & 31));
+}
+
+pcg32_random_t rngstate = PCG32_INITIALIZER;
+
+float rand01()
+{
+	return pcg32_random_r(&rngstate) / float(1ull << 32);
+}
+
+struct State
+{
+	float cache[32];
+	float live[32];
+};
+
+struct Mesh
+{
+	size_t vertex_count;
+	std::vector<unsigned int> indices;
+
+	float atvr_base[Profile_Count];
+};
+
+Mesh gridmesh(unsigned int N)
+{
+	Mesh result;
+
+	result.vertex_count = (N + 1) * (N + 1);
+	result.indices.reserve(N * N * 6);
+
+	for (unsigned int y = 0; y < N; ++y)
+		for (unsigned int x = 0; x < N; ++x)
+		{
+			result.indices.push_back((y + 0) * (N + 1) + (x + 0));
+			result.indices.push_back((y + 0) * (N + 1) + (x + 1));
+			result.indices.push_back((y + 1) * (N + 1) + (x + 0));
+
+			result.indices.push_back((y + 1) * (N + 1) + (x + 0));
+			result.indices.push_back((y + 0) * (N + 1) + (x + 1));
+			result.indices.push_back((y + 1) * (N + 1) + (x + 1));
+		}
+
+	return result;
+}
+
+Mesh objmesh(const char* path)
+{
+	ObjFile file;
+
+	if (!objParseFile(file, path))
+	{
+		printf("Error loading %s: file not found\n", path);
+		return Mesh();
+	}
+
+	if (!objValidate(file))
+	{
+		printf("Error loading %s: invalid file data\n", path);
+		return Mesh();
+	}
+
+	objTriangulate(file);
+
+	size_t total_indices = file.f.size() / 3;
+
+	struct Vertex
+	{
+		float px, py, pz;
+		float nx, ny, nz;
+		float tx, ty;
+	};
+
+	std::vector<Vertex> vertices;
+	vertices.reserve(total_indices);
+
+	for (size_t i = 0; i < file.f.size(); i += 3)
+	{
+		int vi = file.f[i + 0];
+		int vti = file.f[i + 1];
+		int vni = file.f[i + 2];
+
+		Vertex v =
+		    {
+		        file.v[vi * 3 + 0],
+		        file.v[vi * 3 + 1],
+		        file.v[vi * 3 + 2],
+
+		        vni >= 0 ? file.vn[vni * 3 + 0] : 0,
+		        vni >= 0 ? file.vn[vni * 3 + 1] : 0,
+		        vni >= 0 ? file.vn[vni * 3 + 2] : 0,
+
+		        vti >= 0 ? file.vt[vti * 3 + 0] : 0,
+		        vti >= 0 ? file.vt[vti * 3 + 1] : 0,
+		    };
+
+		vertices.push_back(v);
+	}
+
+	Mesh result;
+
+	std::vector<unsigned int> remap(total_indices);
+
+	size_t total_vertices = meshopt_generateVertexRemap(&remap[0], NULL, total_indices, &vertices[0], total_indices, sizeof(Vertex));
+
+	result.indices.resize(total_indices);
+	meshopt_remapIndexBuffer(&result.indices[0], NULL, total_indices, &remap[0]);
+
+	result.vertex_count = total_vertices;
+
+	return result;
+}
+
+void compute_atvr(const State& state, const Mesh& mesh, float result[Profile_Count])
+{
+	memcpy(meshopt::vertex_score_table_cache + 1, state.cache, 32 * sizeof(float));
+	memcpy(meshopt::vertex_score_table_live + 1, state.live, 32 * sizeof(float));
+
+	std::vector<unsigned int> indices(mesh.indices.size());
+
+	meshopt_optimizeVertexCache(&indices[0], &mesh.indices[0], mesh.indices.size(), mesh.vertex_count, 0);
+
+	for (int profile = 0; profile < Profile_Count; ++profile)
+		result[profile] = meshopt_analyzeVertexCache(&indices[0], indices.size(), mesh.vertex_count, profiles[profile].cache, profiles[profile].warp, profiles[profile].triangle).atvr;
+}
+
+float fitness_score(const State& state, const std::vector<Mesh>& meshes)
+{
+	float result = 0;
+	float count = 0;
+
+	for (auto& mesh: meshes)
+	{
+		float atvr[Profile_Count];
+		compute_atvr(state, mesh, atvr);
+
+		for (int profile = 0; profile < Profile_Count; ++profile)
+		{
+			result += mesh.atvr_base[profile] / atvr[profile];
+			count += 1;
+		}
+	}
+
+	return result / count;
+}
+
+float rndcache(int j)
+{
+	return rand01();
+}
+
+float rndlive(int j)
+{
+	return rand01();
+}
+
+std::vector<State> gen0(size_t count)
+{
+	std::vector<State> result;
+
+	for (size_t i = 0; i < count; ++i)
+	{
+		State state = {};
+
+		for (int j = 0; j < 32; ++j)
+		{
+			state.cache[j] = rndcache(j);
+			state.live[j] = rndlive(j);
+		}
+
+		result.push_back(state);
+	}
+
+	return result;
+}
+
+size_t rndindex(const std::vector<float>& prob)
+{
+	float r = rand01();
+
+	for (size_t i = 0; i < prob.size(); ++i)
+	{
+		r -= prob[i];
+
+		if (r <= 0)
+			return i;
+	}
+
+	return prob.size() - 1;
+}
+
+std::pair<State, float> genN(std::vector<State>& seed, const std::vector<Mesh>& meshes, float crossover, float mutate)
+{
+	std::vector<State> result;
+	result.reserve(seed.size());
+
+	std::vector<float> seedprob(seed.size());
+
+	#pragma omp parallel for
+	for (size_t i = 0; i < seed.size(); ++i)
+	{
+		seedprob[i] = fitness_score(seed[i], meshes);
+	}
+
+	State best = {};
+	float bestfit = 0;
+	float probsum = 0;
+
+	for (size_t i = 0; i < seed.size(); ++i)
+	{
+		float score = seedprob[i];
+		probsum += score;
+
+		if (score > bestfit)
+		{
+			best = seed[i];
+			bestfit = score;
+		}
+	}
+
+	for (auto& prob: seedprob)
+	{
+		prob /= probsum;
+	}
+
+	std::vector<unsigned int> seedidx;
+	seedidx.reserve(seed.size());
+	for (size_t i = 0; i < seed.size(); ++i)
+		seedidx.push_back(i);
+
+	std::sort(seedidx.begin(), seedidx.end(), [&](size_t l, size_t r) { return seedprob[l] < seedprob[r]; });
+
+	while (result.size() < seed.size() / 4)
+	{
+		size_t idx = seedidx.back();
+		seedidx.pop_back();
+
+		result.push_back(seed[idx]);
+	}
+
+	while (result.size() < seed.size())
+	{
+		State s0 = seed[rndindex(seedprob)];
+		State s1 = seed[rndindex(seedprob)];
+
+		State state = s0;
+
+		// crossover
+		if (rand01() < crossover)
+		{
+			size_t idxcache = std::min(int(rand01() * 32 + 0.5f), 31);
+			size_t idxlive = std::min(int(rand01() * 32 + 0.5f), 31);
+
+			memcpy(state.cache + idxcache, s1.cache + idxcache, (32 - idxcache) * sizeof(float));
+			memcpy(state.live + idxlive, s1.live + idxlive, (32 - idxlive) * sizeof(float));
+		}
+
+		// mutate
+		if (rand01() < mutate)
+		{
+			size_t idxcache = std::min(int(rand01() * 32 + 0.5f), 31);
+			size_t idxlive = std::min(int(rand01() * 32 + 0.5f), 31);
+
+			state.cache[idxcache] = rndcache(idxcache);
+			state.live[idxlive] = rndlive(idxlive);
+		}
+
+		result.push_back(state);
+	}
+
+	seed.swap(result);
+
+	return std::make_pair(best, bestfit);
+}
+
+bool load_state(const char* path, std::vector<State>& result)
+{
+	FILE* file = fopen(path, "rb");
+	if (!file)
+		return false;
+
+	State state;
+
+	result.clear();
+
+	while (fread(&state, sizeof(State), 1, file) == 1)
+		result.push_back(state);
+
+	fclose(file);
+
+	return true;
+}
+
+bool save_state(const char* path, const std::vector<State>& result)
+{
+	FILE* file = fopen(path, "wb");
+	if (!file)
+		return false;
+
+	for (auto& state: result)
+	{
+		if (fwrite(&state, sizeof(State), 1, file) != 1)
+		{
+			fclose(file);
+			return false;
+		}
+	}
+
+	return fclose(file) == 0;
+}
+
+void dump_state(const State& state)
+{
+	printf("cache:");
+	for (int i = 0; i < 32; ++i)
+	{
+		if (i == 16) printf(" |");
+		printf(" %.3f", state.cache[i]);
+	}
+	printf("\n");
+
+	printf("live:");
+	for (int i = 0; i < 32; ++i)
+	{
+		if (i == 8) printf(" |");
+		printf(" %.3f", state.live[i]);
+	}
+	printf("\n");
+}
+
+int main(int argc, char** argv)
+{
+	State baseline;
+	memcpy(baseline.cache, meshopt::vertex_score_table_cache + 1, 32 * sizeof(float));
+	memcpy(baseline.live, meshopt::vertex_score_table_live + 1, 32 * sizeof(float));
+
+	std::vector<Mesh> meshes;
+
+	meshes.push_back(gridmesh(50));
+
+	for (int i = 1; i < argc; ++i)
+		meshes.push_back(objmesh(argv[i]));
+
+	size_t total_triangles = 0;
+
+	for (auto& mesh: meshes)
+	{
+		compute_atvr(baseline, mesh, mesh.atvr_base);
+
+		total_triangles += mesh.indices.size() / 3;
+	}
+
+	std::vector<State> pop;
+	size_t gen = 0;
+
+	if (load_state("mutator.state", pop))
+	{
+		printf("Loaded %d state vectors\n", int(pop.size()));
+	}
+	else
+	{
+		pop = gen0(1000);
+	}
+
+	printf("%d meshes, %.1fM triangles\n", int(meshes.size()), double(total_triangles) / 1e6);
+
+	float atvr_0[Profile_Count];
+	float atvr_N[Profile_Count];
+	compute_atvr(baseline, meshes[0], atvr_0);
+	compute_atvr(baseline, meshes.back(), atvr_N);
+
+	printf("baseline: grid %f %f %s %f %f\n", atvr_0[Profile_AMD_GCN], atvr_0[Profile_NVidia_Pascal], argv[argc - 1], atvr_N[Profile_AMD_GCN], atvr_N[Profile_NVidia_Pascal]);
+
+	for (;;)
+	{
+		auto best = genN(pop, meshes, 0.7f, 0.3f);
+		gen++;
+
+		compute_atvr(best.first, meshes[0], atvr_0);
+		compute_atvr(best.first, meshes.back(), atvr_N);
+
+		printf("%d: fitness %f; grid %f %f %s %f %f\n", int(gen), best.second, atvr_0[Profile_AMD_GCN], atvr_0[Profile_NVidia_Pascal], argv[argc - 1], atvr_N[Profile_AMD_GCN], atvr_N[Profile_NVidia_Pascal]);
+
+		if (gen % 100 == 0)
+		{
+			char buf[128];
+			sprintf(buf, "gcloud logging write vcache-log \"fitness %f; grid %f %f %s %f %f\"", best.second, atvr_0[Profile_AMD_GCN], atvr_0[Profile_NVidia_Pascal], argv[argc - 1], atvr_N[Profile_AMD_GCN], atvr_N[Profile_NVidia_Pascal]);
+			system(buf);
+		}
+
+		dump_state(best.first);
+
+		if (save_state("mutator.state-temp", pop) && rename("mutator.state-temp", "mutator.state") == 0)
+		{
+		}
+		else
+		{
+			printf("ERROR: Can't save state\n");
+		}
+	}
+}
