5da06905527e06c759b41c363ce465f7531c5a99
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2017-12-25 10:05:13 -0800

    vfetch: Use C-style includes

--- src/vfetchanalyzer.cpp
+++ src/vfetchanalyzer.cpp
@@ -1,7 +1,7 @@
 // This file is part of meshoptimizer library; see meshoptimizer.h for version/license details
 #include "meshoptimizer.h"
 
-#include <cassert>
+#include <assert.h>
 
 meshopt_VertexFetchStatistics meshopt_analyzeVertexFetch(const unsigned int* indices, size_t index_count, size_t vertex_count, size_t vertex_size)
 {
