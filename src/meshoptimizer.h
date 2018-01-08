900b9e35253c6eb24fa96af35e1c154cf599c30f
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2018-01-07 20:35:22 -0800

    Update copyright year to 2018

--- src/meshoptimizer.h
+++ src/meshoptimizer.h
@@ -1,7 +1,7 @@
 /**
  * meshoptimizer - version 0.6
  *
- * Copyright (C) 2016-2017, by Arseny Kapoulkine (arseny.kapoulkine@gmail.com)
+ * Copyright (C) 2016-2018, by Arseny Kapoulkine (arseny.kapoulkine@gmail.com)
  * Report bugs and download new versions at https://github.com/zeux/meshoptimizer
  *
  * This library is distributed under the MIT License. See notice at the end of this file.
@@ -497,7 +497,7 @@ public:
 #endif
 
 /**
- * Copyright (c) 2016-2017 Arseny Kapoulkine
+ * Copyright (c) 2016-2018 Arseny Kapoulkine
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
