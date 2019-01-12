0f9ab3724ba204b6c7b8bcddb655259d2513de7e
Graham Wihlidal <graham@wihlidal.ca>
2019-01-12 10:31:28 +0100

    Optimize meshopt_computeMeshletBounds by passing in pointer to meshlet instead of by value (636 bytes)

--- src/meshoptimizer.h
+++ src/meshoptimizer.h
@@ -323,7 +323,7 @@ struct meshopt_Bounds
  * index_count should be less than or equal to 256*3 (the function assumes clusters of limited size)
  */
 MESHOPTIMIZER_EXPERIMENTAL struct meshopt_Bounds meshopt_computeClusterBounds(const unsigned int* indices, size_t index_count, const float* vertex_positions, size_t vertex_count, size_t vertex_positions_stride);
-MESHOPTIMIZER_EXPERIMENTAL struct meshopt_Bounds meshopt_computeMeshletBounds(struct meshopt_Meshlet meshlet, const float* vertex_positions, size_t vertex_count, size_t vertex_positions_stride);
+MESHOPTIMIZER_EXPERIMENTAL struct meshopt_Bounds meshopt_computeMeshletBounds(const struct meshopt_Meshlet* meshlet, const float* vertex_positions, size_t vertex_count, size_t vertex_positions_stride);
 
 #ifdef __cplusplus
 } /* extern "C" */
