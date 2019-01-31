ed9586d9f0b29c77e9fc5af11a9fa8647d844189
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2019-01-31 08:40:28 -0800

    demo: Fix simplify function
    The previous commit accidentally changed base simplify() to use
    simplifySloppy algorithm.

--- demo/main.cpp
+++ demo/main.cpp
@@ -368,7 +368,7 @@ void simplify(const Mesh& mesh)
 	float target_error = 1e-3f;
 
 	lod.indices.resize(mesh.indices.size());
-	lod.indices.resize(meshopt_simplifySloppy(&lod.indices[0], &mesh.indices[0], mesh.indices.size(), &mesh.vertices[0].px, mesh.vertices.size(), sizeof(Vertex), target_index_count, target_error));
+	lod.indices.resize(meshopt_simplify(&lod.indices[0], &mesh.indices[0], mesh.indices.size(), &mesh.vertices[0].px, mesh.vertices.size(), sizeof(Vertex), target_index_count, target_error));
 
 	lod.vertices.resize(mesh.vertices.size());
 	lod.vertices.resize(meshopt_optimizeVertexFetch(&lod.vertices[0], &lod.indices[0], lod.indices.size(), &mesh.vertices[0], mesh.vertices.size(), sizeof(Vertex)));
