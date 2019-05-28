92848d27aae65ca7cd70f63ab48ac250be8ac046
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2019-05-27 18:12:20 -0700

    gltfpack: Implement mesh parsing
    For now we add all attributes for all meshes in world space,
    deduplicating mesh references as necessary. We only support indexed
    meshes.

--- tools/gltfpack.cpp
+++ tools/gltfpack.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <vector>
 
+#include <math.h>
+
 #define CGLTF_IMPLEMENTATION
 #include "cgltf.h"
 
@@ -14,6 +16,8 @@ struct Attr
 struct Stream
 {
 	cgltf_attribute_type type;
+	int index;
+
 	std::vector<Attr> data;
 };
 
@@ -58,9 +62,104 @@ const char* getError(cgltf_result result)
 	}
 }
 
+cgltf_accessor* getAccessor(const cgltf_attribute* attributes, size_t attribute_count, cgltf_attribute_type type, int index = 0)
+{
+	for (size_t i = 0; i < attribute_count; ++i)
+		if (attributes[i].type == type && attributes[i].index == index)
+			return attributes[i].data;
+
+	return 0;
+}
+
+void transformPosition(float* ptr, const float* transform)
+{
+	float x = ptr[0] * transform[0] + ptr[1] * transform[4] + ptr[2] * transform[8] + transform[12];
+	float y = ptr[0] * transform[1] + ptr[1] * transform[5] + ptr[2] * transform[9] + transform[13];
+	float z = ptr[0] * transform[2] + ptr[1] * transform[6] + ptr[2] * transform[10] + transform[14];
+
+	ptr[0] = x;
+	ptr[1] = y;
+	ptr[2] = z;
+}
+
+void transformNormal(float* ptr, const float* transform)
+{
+	float x = ptr[0] * transform[0] + ptr[1] * transform[4] + ptr[2] * transform[8];
+	float y = ptr[0] * transform[1] + ptr[1] * transform[5] + ptr[2] * transform[9];
+	float z = ptr[0] * transform[2] + ptr[1] * transform[6] + ptr[2] * transform[10];
+
+	float l = sqrtf(x * x + y * y + z * z);
+	float s = (l == 0.f) ? 0.f : 1 / l;
+
+	ptr[0] = x * s;
+	ptr[1] = y * s;
+	ptr[2] = z * s;
+}
+
+void parseMeshes(cgltf_data* data, std::vector<Mesh>& meshes)
+{
+	for (size_t ni = 0; ni < data->nodes_count; ++ni)
+	{
+		if (!data->nodes[ni].mesh)
+			continue;
+
+		float transform[16];
+		cgltf_node_transform_world(&data->nodes[ni], transform);
+
+		const cgltf_mesh& mesh = *data->nodes[ni].mesh;
+
+		for (size_t pi = 0; pi < mesh.primitives_count; ++pi)
+		{
+			const cgltf_primitive& primitive = mesh.primitives[pi];
+
+			Mesh result;
+
+			if (cgltf_accessor* a = primitive.indices)
+			{
+				result.indices.resize(a->count);
+				for (size_t i = 0; i < a->count; ++i)
+					result.indices[i] = cgltf_accessor_read_index(a, i);
+			}
+
+			for (size_t ai = 0; ai < primitive.attributes_count; ++ai)
+			{
+				const cgltf_attribute& attr = primitive.attributes[ai];
+
+				if (attr.type == cgltf_attribute_type_invalid)
+					continue;
+
+				Stream s = { attr.type, attr.index };
+				s.data.resize(attr.data->count);
+
+				for (size_t i = 0; i < attr.data->count; ++i)
+					cgltf_accessor_read_float(attr.data, i, s.data[i].f, 4);
+
+				if (attr.type == cgltf_attribute_type_position)
+				{
+					for (size_t i = 0; i < attr.data->count; ++i)
+						transformPosition(s.data[i].f, transform);
+				}
+				else if (attr.type == cgltf_attribute_type_normal || attr.type == cgltf_attribute_type_tangent)
+				{
+					for (size_t i = 0; i < attr.data->count; ++i)
+						transformNormal(s.data[i].f, transform);
+				}
+
+				result.streams.push_back(s);
+			}
+
+			if (result.indices.size() && result.streams.size())
+				meshes.push_back(result);
+		}
+	}
+}
+
 bool process(Scene& scene, std::string& json, std::string& bin)
 {
-	(void)scene;
+	cgltf_data* data = scene.data;
+
+	parseMeshes(data, scene.meshes);
+
 	(void)json;
 	(void)bin;
 	return true;
