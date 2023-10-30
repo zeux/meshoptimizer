69844209fcbfb432db8e70f5ed883674b8a4b973
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2023-10-29 17:58:16 -0700

    js: Mark simplifyWithAttributes and simplifyPoints as experimental
    Both functions are marked as experimental in C++ code; their interface
    and implementation is subject to change. In JS, if the interface of
    these functions changes, it may lead to subtle or not-so-subtle issues
    with these functions. For now we require the caller to opt into the
    use of these functions by setting a module-level experimental boolean.

--- js/meshopt_simplifier.module.d.ts
+++ js/meshopt_simplifier.module.d.ts
@@ -5,14 +5,18 @@ export type Flags = "LockBorder";
 export const MeshoptSimplifier: {
     supported: boolean;
     ready: Promise<void>;
+
+    useExperimentalFeatures: boolean;
     
     compactMesh: (indices: Uint32Array) => [Uint32Array, number];
     
     simplify: (indices: Uint32Array, vertex_positions: Float32Array, vertex_positions_stride: number, target_index_count: number, target_error: number, flags?: Flags[]) => [Uint32Array, number];
 
+    // Experimental; requires useExperimentalFeatures to be set to true
     simplifyWithAttributes: (indices: Uint32Array, vertex_positions: Float32Array, vertex_positions_stride: number, vertex_attributes: Float32Array, vertex_attributes_stride: number, attribute_weights: number[], target_index_count: number, target_error: number, flags?: Flags[]) => [Uint32Array, number];
 
     getScale: (vertex_positions: Float32Array, vertex_positions_stride: number) => number;
 
+    // Experimental; requires useExperimentalFeatures to be set to true
     simplifyPoints: (vertex_positions: Float32Array, vertex_positions_stride: number, target_vertex_count: number, vertex_colors?: Float32Array, vertex_colors_stride?: number, color_weight?: number) => Uint32Array;
 };
