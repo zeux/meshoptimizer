3dc0706c105e71259819ea48a9895f64ad10cd9b
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2024-08-30 11:09:48 -0700

    Update simplification documentation to remove shadowIndexBuffer
    While both the header comments and README recommented
    meshopt_generateShadowIndexBuffer for pre-welding during simplification,
    this guidance was incorrect: simplifying a shadow index buffer is
    currently subject to some of the same restrictions as the main index
    buffer because the classification is partially based on the contents of
    the entire vertex buffer including unreferenced vertices.
    
    This could change in the future, and can be worked around via
    meshopt_SimplifySparse, but because it's not clear if this is even a
    preferred route going forward, for now remove all incorrect suggestions
    and keep meshopt_generateShadowIndexBuffer for its narrow use of
    generating optimal index buffers for depth only rendering.

--- src/meshoptimizer.h
+++ src/meshoptimizer.h
@@ -342,7 +342,7 @@ enum
  * Mesh simplifier
  * Reduces the number of triangles in the mesh, attempting to preserve mesh appearance as much as possible
  * The algorithm tries to preserve mesh topology and can stop short of the target goal based on topology constraints or target error.
- * If not all attributes from the input mesh are required, it's recommended to reindex the mesh using meshopt_generateShadowIndexBuffer prior to simplification.
+ * If not all attributes from the input mesh are required, it's recommended to reindex the mesh without them prior to simplification.
  * Returns the number of indices after simplification, with destination containing new index data
  * The resulting index buffer references vertices from the original vertex buffer.
  * If the original vertex data isn't required, creating a compact vertex buffer using meshopt_optimizeVertexFetch is recommended.
