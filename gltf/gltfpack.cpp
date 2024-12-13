cb92e50ce9dfd8ada533df0d3f28b5d158addbae
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2024-12-12 18:12:11 -0800

    gltfpack: Add -ce option for experimental compression
    This is currently under NDEBUG because the format is not binary stable,
    but this allows to easily test the compression improvements on arbitrary
    glTF files.

--- gltf/gltfpack.cpp
+++ gltf/gltfpack.cpp
@@ -1514,6 +1514,15 @@ int main(int argc, char** argv)
 			settings.compress = true;
 			settings.fallback = true;
 		}
+#ifndef NDEBUG
+		else if (strcmp(arg, "-ce") == 0)
+		{
+			fprintf(stderr, "Warning: experimental compression will produce files that may not decode in the future!\n");
+			meshopt_encodeVertexVersion(0xe);
+			settings.compress = true;
+			settings.compressmore = true;
+		}
+#endif
 		else if (strcmp(arg, "-v") == 0)
 		{
 			settings.verbose = 1;
