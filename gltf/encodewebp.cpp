c65d6042075b3d6a1c2097450d6d47296bb21caa
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2025-08-17 15:18:19 -0700

    gltfpack: Simplify WebP encoding implementation
    This code is mechanically the same, but it uses unique_ptr to simplify
    memory management, and a custom write function to write directly to
    target std::string.

--- gltf/encodewebp.cpp
+++ gltf/encodewebp.cpp
@@ -9,8 +9,21 @@
 #include "imageio/image_dec.h"
 #endif
 
-static const char* prepareEncodeWebP(const cgltf_image& image, const char* input_path, const ImageInfo& info, const Settings& settings, std::string& webp_output)
+#include <memory>
+
+static int writeWebP(const uint8_t* data, size_t data_size, const WebPPicture* picture)
 {
+	std::string* encoded = static_cast<std::string*>(picture->custom_ptr);
+	encoded->append(reinterpret_cast<const char*>(data), data_size);
+	return 1;
+}
+
+static const char* encodeWebP(const cgltf_image& image, const char* input_path, const ImageInfo& info, const Settings& settings, std::string& encoded)
+{
+	WebPConfig config;
+	if (!WebPConfigInit(&config))
+		return "error initializing WebP";
+
 	std::string img_data;
 	std::string mime_type;
 
@@ -26,62 +39,30 @@ static const char* prepareEncodeWebP(const cgltf_image& image, const char* input
 
 	adjustDimensions(width, height, settings.texture_scale[info.kind], settings.texture_limit[info.kind], settings.texture_pow2);
 
-	WebPPicture pic;
-	if (!WebPPictureInit(&pic))
-		return "error initializing WebP picture";
-
-	WebPInputFileFormat format;
-	if (mime_type == "image/png")
-		format = WEBP_PNG_FORMAT;
-	else if (mime_type == "image/jpeg")
-		format = WEBP_JPEG_FORMAT;
-	else
-		return NULL;
-
+	WebPInputFileFormat format = (mime_type == "image/jpeg") ? WEBP_JPEG_FORMAT : WEBP_PNG_FORMAT;
 	WebPImageReader reader = WebPGetImageReader(format);
 	if (!reader)
-		return "error getting WebP image reader";
+		return "unsupported image format";
+
+	WebPPicture pic;
+	if (!WebPPictureInit(&pic))
+		return "error initializing picture";
+
+	std::unique_ptr<WebPPicture, void (*)(WebPPicture*)> pic_storage(&pic, WebPPictureFree);
 
 	if (!reader((const uint8_t*)img_data.data(), img_data.size(), &pic, 1, NULL))
-	{
-		WebPPictureFree(&pic);
 		return "error decoding source image";
-	}
 
-	if (width != pic.width || height != pic.height)
-	{
-		if (!WebPPictureRescale(&pic, width, height))
-		{
-			WebPPictureFree(&pic);
-			return "error resizing image";
-		}
-	}
+	if ((width != pic.width || height != pic.height) && !WebPPictureRescale(&pic, width, height))
+		return "error resizing image";
 
-	WebPConfig config;
-	if (!WebPConfigInit(&config))
-	{
-		WebPPictureFree(&pic);
-		return "error initializing WebP config";
-	}
-
-	config.lossless = 0;
 	config.quality = settings.texture_quality[info.kind] * 10; // map 1-10 to 10-100
 
-	WebPMemoryWriter writer;
-	WebPMemoryWriterInit(&writer);
-	pic.writer = WebPMemoryWrite;
-	pic.custom_ptr = &writer;
+	pic.writer = writeWebP;
+	pic.custom_ptr = &encoded;
 
 	if (!WebPEncode(&config, &pic))
-	{
-		WebPMemoryWriterClear(&writer);
-		WebPPictureFree(&pic);
-		return "error encoding WebP image";
-	}
-
-	webp_output.assign((const char*)writer.mem, writer.size);
-	WebPMemoryWriterClear(&writer);
-	WebPPictureFree(&pic);
+		return "error encoding image";
 
 	return NULL;
 }
@@ -96,10 +77,8 @@ void encodeImagesWebP(std::string* encoded, const cgltf_data* data, const std::v
 		if (settings.texture_mode[info.kind] != TextureMode_WebP)
 			continue;
 
-		if (const char* error = prepareEncodeWebP(image, input_path, info, settings, encoded[i]))
-		{
+		if (const char* error = encodeWebP(image, input_path, info, settings, encoded[i]))
 			encoded[i] = error;
-		}
 	}
 }
 
