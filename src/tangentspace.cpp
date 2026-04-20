4e9bffceb0ec1db9f9cc7cf6084ff0ac96e840d1
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2026-04-19 20:49:22 -0700

    tangentspace: Compute vertex remap to deduplicate input vertices
    Since the input vertices are not guaranteed to have any particular
    structure, including possibility of unindexed input or vertex splits due
    to non-tangent-related attributes, we need to deduplicate them. Since
    MikkTSpace relies on exact equality for this deduplication, we can use a
    hash map to achieve the same goal faster.
    
    For now we use a binary hash (Murmur) for the individual components and
    don't handle negative zero; this will be adjusted later.

--- src/tangentspace.cpp
+++ src/tangentspace.cpp
@@ -58,6 +58,123 @@ static void computeFaceTangents(Tangent* result, size_t triangle_count, const un
 	}
 }
 
+static unsigned int hashUpdate4(unsigned int h, const unsigned char* key, size_t len)
+{
+	// MurmurHash2
+	const unsigned int m = 0x5bd1e995;
+	const int r = 24;
+
+	while (len >= 4)
+	{
+		unsigned int k = *reinterpret_cast<const unsigned int*>(key);
+
+		k *= m;
+		k ^= k >> r;
+		k *= m;
+
+		h *= m;
+		h ^= k;
+
+		key += 4;
+		len -= 4;
+	}
+
+	return h;
+}
+
+struct VertexHasherF
+{
+	const float* vertex_positions;
+	size_t vertex_positions_stride_float;
+	const float* vertex_normals;
+	size_t vertex_normals_stride_float;
+	const float* vertex_uvs;
+	size_t vertex_uvs_stride_float;
+
+	size_t hash(unsigned int index) const
+	{
+		const float* p = vertex_positions + index * vertex_positions_stride_float;
+		const float* n = vertex_normals + index * vertex_normals_stride_float;
+		const float* t = vertex_uvs + index * vertex_uvs_stride_float;
+
+		// TODO: use a simpler / more direct hash function + handle negative zero
+		unsigned int hash = 0;
+		hash = hashUpdate4(hash, reinterpret_cast<const unsigned char*>(p), 3 * sizeof(float));
+		hash = hashUpdate4(hash, reinterpret_cast<const unsigned char*>(n), 3 * sizeof(float));
+		hash = hashUpdate4(hash, reinterpret_cast<const unsigned char*>(t), 2 * sizeof(float));
+		return hash;
+	}
+
+	bool equal(unsigned int lhs, unsigned int rhs) const
+	{
+		const float* lp = vertex_positions + lhs * vertex_positions_stride_float;
+		const float* ln = vertex_normals + lhs * vertex_normals_stride_float;
+		const float* lt = vertex_uvs + lhs * vertex_uvs_stride_float;
+		const float* rp = vertex_positions + rhs * vertex_positions_stride_float;
+		const float* rn = vertex_normals + rhs * vertex_normals_stride_float;
+		const float* rt = vertex_uvs + rhs * vertex_uvs_stride_float;
+		return lp[0] == rp[0] && lp[1] == rp[1] && lp[2] == rp[2] && ln[0] == rn[0] && ln[1] == rn[1] && ln[2] == rn[2] && lt[0] == rt[0] && lt[1] == rt[1];
+	}
+};
+
+static size_t hashBuckets4(size_t count)
+{
+	size_t buckets = 1;
+	while (buckets < count + count / 4)
+		buckets *= 2;
+
+	return buckets;
+}
+
+template <typename T, typename Hash>
+static T* hashLookup4(T* table, size_t buckets, const Hash& hash, const T& key, const T& empty)
+{
+	assert(buckets > 0);
+	assert((buckets & (buckets - 1)) == 0);
+
+	size_t hashmod = buckets - 1;
+	size_t bucket = hash.hash(key) & hashmod;
+
+	for (size_t probe = 0; probe <= hashmod; ++probe)
+	{
+		T& item = table[bucket];
+
+		if (item == empty)
+			return &item;
+
+		if (hash.equal(item, key))
+			return &item;
+
+		// hash collision, quadratic probing
+		bucket = (bucket + probe + 1) & hashmod;
+	}
+
+	assert(false && "Hash table is full"); // unreachable
+	return NULL;
+}
+
+static void buildVertexRemap(unsigned int* remap, const float* vertex_positions, size_t vertex_count, size_t vertex_positions_stride, const float* vertex_normals, size_t vertex_normals_stride, const float* vertex_uvs, size_t vertex_uvs_stride, meshopt_Allocator& allocator)
+{
+	VertexHasherF vertex_hasher = {vertex_positions, vertex_positions_stride / sizeof(float), vertex_normals, vertex_normals_stride / sizeof(float), vertex_uvs, vertex_uvs_stride / sizeof(float)};
+
+	size_t vertex_table_size = hashBuckets4(vertex_count);
+	unsigned int* vertex_table = allocator.allocate<unsigned int>(vertex_table_size);
+	memset(vertex_table, -1, vertex_table_size * sizeof(unsigned int));
+
+	for (size_t i = 0; i < vertex_count; ++i)
+	{
+		unsigned int index = unsigned(i);
+		unsigned int* entry = hashLookup4(vertex_table, vertex_table_size, vertex_hasher, index, ~0u);
+
+		if (*entry == ~0u)
+			*entry = index;
+
+		remap[index] = *entry;
+	}
+
+	allocator.deallocate(vertex_table);
+}
+
 } // namespace meshopt
 
 // TODO: argument order?
@@ -79,6 +196,10 @@ MESHOPTIMIZER_EXPERIMENTAL void meshopt_generateTangentsMikkT(float* result, con
 	(void)vertex_count;
 	(void)vertex_normals;
 	(void)vertex_normals_stride;
+
+	// compute vertex remap to unique vertex index
+	unsigned int* remap = allocator.allocate<unsigned int>(vertex_count);
+	buildVertexRemap(remap, vertex_positions, vertex_count, vertex_positions_stride, vertex_normals, vertex_normals_stride, vertex_uvs, vertex_uvs_stride, allocator);
 
 	// compute per-triangle tangents
 	Tangent* face_tangents = allocator.allocate<Tangent>(face_count);
