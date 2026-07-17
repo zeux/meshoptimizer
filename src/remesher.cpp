75c35cfbd08d8773633f9ac3abd0e5d67da0dad4
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2026-07-16 21:52:35 -0700

    remesher: Implement initial version of the voxelizer
    In this pass, we only tag the voxels that intersect any triangles. We
    will need more information in boundary voxels to be able to mesh the
    shape accurately, but that requires a lot more storage which should not
    scale as N^3.
    
    For the implementation, for now instead of doing precise box-triangle
    intersection tests, we estimate a barycentric sampling grid and splat
    the points into voxels instead. This should guarantee hole-free
    voxelization as long as the point density is high enough that the
    distance between the two points is never more than a voxel's edge along
    any axis.
    
    For thin triangles, we currently over-sample which might be unreasonably
    slow; this and some other inefficiencies could be fixed later.

--- src/remesher.cpp
+++ src/remesher.cpp
@@ -57,6 +57,58 @@ static float measureGrid(const float* vertex_positions_data, size_t vertex_count
 
 static void voxelize(unsigned char* grid, const unsigned int* indices, size_t index_count, const float* vertex_positions, size_t vertex_count, size_t vertex_positions_stride, int resolution, float scale, const float offset[3])
 {
+	(void)vertex_count;
+
+	size_t vertex_stride_float = vertex_positions_stride / sizeof(float);
+
+	for (size_t i = 0; i < index_count; i += 3)
+	{
+		unsigned int a = indices[i + 0], b = indices[i + 1], c = indices[i + 2];
+		assert(a < vertex_count && b < vertex_count && c < vertex_count);
+
+		const float* va = vertex_positions + a * vertex_stride_float;
+		const float* vb = vertex_positions + b * vertex_stride_float;
+		const float* vc = vertex_positions + c * vertex_stride_float;
+
+		float ex = vb[0] - va[0], ey = vb[1] - va[1], ez = vb[2] - va[2];
+		float fx = vc[0] - va[0], fy = vc[1] - va[1], fz = vc[2] - va[2];
+		float gx = vc[0] - vb[0], gy = vc[1] - vb[1], gz = vc[2] - vb[2];
+
+		// use maximum edge length to establish sampling rate
+		// TODO: this is wasteful for thin triangles
+		float el = sqrtf(ex * ex + ey * ey + ez * ez);
+		float fl = sqrtf(fx * fx + fy * fy + fz * fz);
+		float gl = sqrtf(gx * gx + gy * gy + gz * gz);
+
+		float max_edge = el > fl ? el : fl;
+		max_edge = max_edge > gl ? max_edge : gl;
+
+		// we target 2 samples per voxel edge which should be enough to hit all voxels at any rotation
+		int samples = int(max_edge * scale * 2.f);
+		samples = samples > 1 ? samples : 1;
+		samples = samples < resolution * 2 ? samples : resolution * 2;
+
+		float sx = va[0] - offset[0], sy = va[1] - offset[1], sz = va[2] - offset[2];
+		float sr = 1.f / float(samples);
+
+		for (int u = 0; u <= samples; ++u)
+			for (int v = 0; v <= samples - u; ++v)
+			{
+				float su = float(u) * sr, sv = float(v) * sr;
+				int x = int((sx + su * ex + sv * fx) * scale);
+				int y = int((sy + su * ey + sv * fy) * scale);
+				int z = int((sz + su * ez + sv * fz) * scale);
+
+				// safety: rounding errors and non-finite inputs may produce out of bounds coordinates, so we clamp them
+				// TODO: codegen
+				x = (x < 0) ? 0 : (x > resolution - 2 ? resolution - 2 : x);
+				y = (y < 0) ? 0 : (y > resolution - 2 ? resolution - 2 : y);
+				z = (z < 0) ? 0 : (z > resolution - 2 ? resolution - 2 : z);
+
+				size_t index = (x + 1) + size_t(resolution) * ((y + 1) + size_t(resolution) * (z + 1));
+				grid[index] = 1;
+			}
+	}
 }
 
 } // namespace meshopt
